48ce04d3fa657b774768d63522e046deb5f1e868
muryoimpl <muryoimpl@gmail.com>
2018-12-07 01:33:56 +0900

    Show webpack.config.js to STDOUT via node command

--- lib/webpacker_config_shower.rb
+++ lib/webpacker_config_shower.rb
@@ -1,5 +1,31 @@
-require "webpacker_config_shower/railtie"
+require 'webpacker'
+require 'webpacker/runner'
+require 'open3'
 
 module WebpackerConfigShower
-  # Your code goes here...
+  class Runner < ::Webpacker::Runner
+    def self.run(argv)
+      ENV["NODE_ENV"] ||= Rails.env
+      super
+    end
+
+    def run
+      cmd = [
+        "node",
+        "-e",
+        *[
+          "const conf = require('#{@webpack_config}')",
+          "const { format } = require('util')",
+          "console.log(format('%j', conf))",
+        ].join(';')
+      ]
+
+      Dir.chdir(@app_path) do
+        _, stdout, _ = *Open3.popen3(*cmd)
+        puts JSON.pretty_generate(JSON.parse(stdout.read))
+      end
+    end
+  end
 end
+
+require "webpacker_config_shower/railtie"
